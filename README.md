10a2a9b65150207b8e3ac86478d403295045f5cf
flukehermit <zayed.humayun@g.bracu.ac.bd>
2021-06-06 19:51:52 +0600

    FEAT: Added more options

---
+++ README.md
@@ -0,0 +1,30 @@
+#  Rock-Paper-Scissors
+A classical game "rock, paper and scissors" but with an extended version of the game with customizable options. </br>
+
+The user will first enter their name, then they can choose what options they want to include in the game with comma.
+```
+rock,fire,scissors,snake,human,tree,wolf,sponge,paper,air,water,dragon,devil,lightning,gun
+```
+The list can be of any length and is customizable. </br>
+For playing the classic game only `rock, paper and scissors` the user can enter an empty line.</br>
+The score of the users are upadated in a text file, so they can keep a track of their winnnings.
+
+
+## Syntax
+```
+python game.py
+```
+## Commands
+
+
+|**Commands**|         **Description**        | 
+| ------------- | --------------------------- | 
+| !rating       | Prints your current rating  | 
+| !exit         | Ends the game               |  
+
+## Rules
+- For each draw, 50 points is added to the score of the user.  
+- For each win, add 100 points is added to the score of the user. 
+- In case the user loses, the score does not change.
+<img src="https://i.imgur.com/Ovj1WST.jpg"/>
+
--- game.py
+++ game.py
@@ -1,28 +1,40 @@
 from random import choice
 
-moves = ['rock','paper','scissors']
-commands = ['!exit','!rating']
+moves = ['rock', 'paper', 'scissors']
+commands = ['!exit', '!rating']
 score_bank = {}
 score = 0
 
-def play(player: str,user_move: str, ai_move: str) -> str:
+
+def play(player: str, options: str, user_move: str, ai_move: str) -> str:
     global score
     if user_move == ai_move:
         s = score_bank.setdefault(player, score)
         score_bank[player] = s + 50
+        write_score(player)
         return f"There is a draw ({user_move})"
     else:
-        user_index = moves.index(user_move)
-        ai_index = moves.index(ai_move)
+        user_index = options.index(user_move)
+        rev_list = options[user_index + 1:] + options[:user_index]
+        index_sep = int(len(rev_list) / 2)
+        winning_list = rev_list[:index_sep]
+        loosing_list = rev_list[index_sep:]
 
-        winning_condition = (ai_index + 1) % 3
-
-        if winning_condition == user_index:
+        if ai_move in loosing_list:
             s = score_bank.setdefault(player, score)
             score_bank[player] = s + 100
+            write_score(player)
             return f"Well done. The computer chose {ai_move} and failed"
         else:
             return f"Sorry, but the computer chose {ai_move}"
+
+        #using circular array
+        # offset = (options.index(ai_move) - options.index(user_move)) % len(options)
+        # if offset > len(options) // 2:
+        #     score += 100
+        #     print(f'Well done. Computer chose {ai_move} and failed')
+        # else:
+        #     print(f'Sorry, but computer chose {ai_move}')
 
 def read_score() -> None:
     with open('rating.txt', 'r') as f:
@@ -30,30 +42,59 @@ def read_score() -> None:
             name, score = line.split()
             score_bank[name] = int(score)
 
-def play_game(player: str) -> None:
+
+def write_score(player: str) -> None:
+    idx = None
+    with open('rating.txt', 'r') as file:
+    # read a list of lines into data
+        data = file.readlines()
+        for line in data:
+            if line.split()[0] == player:
+                idx = data.index(line)
+                points = int(line.split()[1])
+
+    if idx is not None:
+        data[idx] = f'{player} {score_bank.setdefault(player, score)}\n'
+        with open('rating.txt', 'w') as file:
+            file.writelines(data)
+    else:
+        with open('rating.txt', 'a') as file:
+            file.write(f"{player} {score_bank.setdefault(player, score)}")
+
+
+
+def play_game(player: str, options: list) -> None:
     while True:
         user_move = input()
 
-        if user_move not in moves and user_move not in commands:
-            print("Invalid input")
-        elif user_move == "!exit":
-             print("Bye!")
-             break
+        if user_move == "!exit":
+            print("Bye!")
+            break
         elif user_move == "!rating":
             s = score_bank.setdefault(player, score)
             print('Your rating:', s)
+        elif user_move not in options:
+            print('Invalid input')
         else:
             ai_move = choice(moves)
-            print(play(player, user_move, ai_move))
+            print(play(player, options, user_move, ai_move))
+
 
 def main() -> None:
     bank = open('rating.txt', 'r')
     read_score()
     current_player = input('Enter your name: ')
     print(f'Hello, {current_player}')
+    options = input()
+    if options == '':
+        options = moves
+    else:
+        options = options.split(",")
+    print("Okay, let's start")
     if current_player not in bank:
         score_bank.setdefault(current_player, score)
     score_bank.update(bank)
-    play_game(current_player)
+    play_game(current_player, options)
+
 
 main()
